2d6b0e5e14f67187abb7a0eaa604101463d99de6
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-10-18 15:03:19 +0300

    build: add nix-shell handling in tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -1,10 +1,12 @@
+if [ -z "$IN_NIX_SHELL" ]; then
+    exec nix-shell --run "$0"
+fi
+
 if tmux has-session -t vox 2>/dev/null; then
-    echo "Session 'vox' already exists. Attaching..."
     tmux attach -t vox
 else
-    echo "Creating new session 'vox'..."
     tmux new-session -ds vox -n root
-    tmux send-keys -t vox:root "echo 'Root dir of the project'" C-m
+    # tmux send-keys -t vox:root "echo 'Root dir of the project'" C-m
     tmux new-window -t vox -n app
     tmux set-option -t vox status-style fg=white,bg=black
     tmux attach -t vox
