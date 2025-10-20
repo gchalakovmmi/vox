4e725205149f79680fdb210787c4b63deea6f362
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-10-20 03:09:24 +0300

    feat: add a config package and slog logging

--- app/cmd/app/main.go
+++ app/cmd/app/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"log/slog"
+	"vox/internal/config"
 )
 
 func main() {
-	fmt.Print("Hello World")
+	cfg := config.New()
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: cfg.LogLevel}))
+	slog.SetDefault(logger)
+	slog.Info("Environment Variables", "PORT", cfg.Port, "INSTANCE_NAME", cfg.InstanceName, "LOG_LEVEL", cfg.LogLevel)
 }
---
+++ app/internal/config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"log/slog"
+	"os"
+)
+
+type Config struct {
+	Port		string
+	InstanceName	string
+	LogLevel	slog.Level
+}
+
+func New() *Config {
+	c := &Config{}
+	c.SetPort()
+	c.SetInstanceName()
+	c.SetLogLevel()
+	return c
+}
+
+func (c *Config) SetPort() {
+	c.Port = os.Getenv("PORT")
+	if c.Port == "" {
+		slog.Error("Environment variable 'PORT' is not set! Exiting...")
+		os.Exit(1)
+	}
+}
+
+func (c *Config) SetInstanceName() {
+	c.InstanceName = os.Getenv("INSTANCE_NAME")
+	if c.InstanceName == "" {
+		slog.Error("Environment variable 'InstanceName' is not set! Exiting...")
+		os.Exit(1)
+	}
+}
+
+func (c *Config) SetLogLevel() {
+	level := os.Getenv("LOG_LEVEL")
+	if level == "" {
+		slog.Error("Environment variable 'LogLevel' is not set! Exiting...")
+		os.Exit(1)
+	}
+	switch level {
+		case "DEBUG":
+			c.LogLevel = slog.LevelDebug
+		case "INFO":
+			c.LogLevel = slog.LevelInfo
+		case "WARN":
+			c.LogLevel = slog.LevelWarn
+		case "ERROR":
+			c.LogLevel = slog.LevelError
+		default:
+			c.LogLevel = slog.LevelInfo
+	}
+}
