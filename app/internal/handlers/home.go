f23a090271ab5c396eae57308761ad3c6086f87d
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-11-24 00:34:52 +0200

    refactor: move WithAuthentication out from home

--- app/internal/handlers/home.go
+++ app/internal/handlers/home.go
@@ -6,20 +6,12 @@ import (
 	"vox/web/templates/pages/home"
 	up "vox/internal/userProfile"
 	sm "vox/internal/status_message"
+	"vox/internal/auth"
 )
-
-
-// Temporairly to let me compile
-func WithAuthentication(next func(up.UserProfile) http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		userProfile := up.UserProfile{}
-		next(userProfile).ServeHTTP(w, r)
-	})
-}
 
 func Home(page string, title string) http.Handler {
 	return sm.WithStatusMessage(func(statusMessage sm.StatusMessage) http.Handler {
-		return WithAuthentication(func(userProfile up.UserProfile) http.Handler {
+		return auth.WithAuthentication(func(userProfile up.UserProfile) http.Handler {
 			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				templ.Handler(home.Handler(page, title, statusMessage, &userProfile)).ServeHTTP(w, r)
 			})
