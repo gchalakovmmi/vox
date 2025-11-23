36f2b4562b5a8cede0d4fc919b027e4c44de575d
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-11-23 19:08:33 +0200

    fix: remove environment variable getting

--- app/internal/postgres/postgres.go
+++ app/internal/postgres/postgres.go
@@ -7,70 +7,34 @@ import (
 	_ "github.com/lib/pq"
 	"log/slog"
 	"net/http"
+	"vox/internal/config"
 )
 
 const (
     PGErrUniqueViolation = "23505"
 )
 
-type DB struct {
-	user			string
-	password		string
-	dbName			string
-	host			string
-	connectionString	string
-	conn			*sql.DB
-}
-
-func NewDB() (*DB, error){
-	db := DB{
-		user:		os.Getenv("POSTGRES_USER"),
-		password:	os.Getenv("POSTGRES_PASSWORD"),
-		dbName:		os.Getenv("POSTGRES_DB"),
-		host:		os.Getenv("POSTGRES_HOST"),
-	}
-
-	db.connectionString = fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=disable",
-		db.user, db.password, db.dbName, db.host)
-
-	var err error
-	db.conn, err = sql.Open("postgres", db.connectionString)
-	if err != nil {
-		return nil, err
-	}
-
-	if err := db.conn.Ping(); err != nil {
-		db.conn.Close()
-		return nil, fmt.Errorf("failed to ping database: %w", err)
-	}
-	
-	return &db, nil
-}
-
-func (db *DB) Conn() (*sql.DB){
-	return db.conn
-}
-
-func (db *DB) Close() error {
-	if db.conn != nil {
-		return db.conn.Close()
-	}
-	return nil
-}
-
-func WithDBConn(next func(*sql.DB) http.Handler) http.Handler {
+func WithDBConn(cfg *config.Config, next func(*sql.DB) http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		db, err := NewDB()
+		conn, err := sql.Open("postgres",
+			fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=disable",
+				cfg.GetPostgresUser(),
+				cfg.GetPostgresPassword(),
+				cfg.GetPostgresDB(),
+				cfg.GetPostgresHost()))
 		if err != nil {
 			slog.Error("internal/middleware/postgres.go", "Message", "Connecting to DB failed", "Error", err)
 			http.Error(w, "Database connection failed", http.StatusInternalServerError)
 			return
 		}
-		defer db.Close()
+		defer conn.Close()
 
-		conn := db.Conn()
-		slog.Debug("internal/middleware/postgres.go", "Message", "DB Connection Successful")
-		
+		if err := conn.Ping(); err != nil {
+			slog.Error("internal/middleware/postgres.go", "Message", "Connecting to DB failed", "Error", err)
+			http.Error(w, "Database ping failed", http.StatusInternalServerError)
+			return
+		}
+
 		next(conn).ServeHTTP(w, r)
 	})
 }
