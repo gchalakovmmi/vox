f2d484aa51b6e908be0eba044d8c5f7fa46428fd
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-11-23 19:01:24 +0200

    feat: add JWT elements in server.go

--- app/internal/server/server.go
+++ app/internal/server/server.go
@@ -3,19 +3,25 @@ package server
 import (
 	"net/http"
 	"vox/internal/config"
+	"vox/internal/handlers"
+	"database/sql"
+	"github.com/redis/go-redis/v9"
+	"vox/internal/auth"
 )
 
 type Server struct {
 	config	*config.Config
-	// db	*sql.DB
-	routes	[]Route
+	db     *sql.DB
+	rdb    *redis.Client
 }
 
-func New(cfg *config.Config, /*db *sqlSomething,*/ routes []Route) *Server {
-	return &Server{
-		config: cfg,
-		routes:	routes,
-	}
+type Route struct {
+	Endpoint	string
+	Handler		http.Handler
+}
+
+func New(cfg *config.Config, db *sql.DB, rdb *redis.Client) *Server {
+    return &Server{config: cfg, db: db, rdb: rdb}
 }
 
 func (srv *Server) CreateHandlers() http.Handler {
@@ -23,9 +29,17 @@ func (srv *Server) CreateHandlers() http.Handler {
 
 	mux.Handle("/web/static/", http.StripPrefix("/web/static/", http.FileServer(http.Dir("./web/static/"))))
 
-	for _, route := range srv.routes {
-		mux.Handle(route.Endpoint, route.Handler)
-	}
-	
+	mux.Handle("/health",		handlers.Health())
+	mux.Handle("/signin",		handlers.Signin("signin", "Sign In"))
+	mux.Handle("/process-signin",	handlers.ProcessSignin(srv.config))
+	mux.Handle("/signup",		handlers.Signup("signup", "Sign Up"))
+	mux.Handle("/process-signup",	handlers.ProcessSignup(srv.config))
+	mux.Handle("/home",		auth.MiddlewareAccessToken(srv.rdb)(handlers.Home("home", "Home")))
+
+	auth.RegisterAuthRoutes(mux, srv.db, srv.rdb)
+	mux.Handle("/auth/", http.StripPrefix("/auth", mux))
+
+	mux.Handle("/",			handlers.Home("home", "Home"))
+
 	return mux
 }
