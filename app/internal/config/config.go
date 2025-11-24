28011cc643a50b80984af03c2b7d6fbf0e96d14f
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-11-24 14:13:39 +0200

    feat: add TTS STT and LLM env variables and reading them

--- app/internal/config/config.go
+++ app/internal/config/config.go
@@ -29,6 +29,11 @@ type Config struct {
 
 	CookieSecure		bool
 	CookieSameSite		http.SameSite
+
+	TTSOpenAIURL		string
+	TTSOpenAIModel		string
+	TTSOpenAIVoice		string
+	TTSOpenAIKey		string
 }
 
 func (c *Config) Set(key string, dest *string) {
@@ -75,47 +80,50 @@ func (c *Config) SetLogLevel() {
 }
 
 func (c *Config) SetBool(key string, dest *bool) {
-    val := strings.TrimSpace(os.Getenv(key))
-    switch strings.ToLower(val) {
-    case "true":
-        *dest = true
-    case "false":
-        *dest = false
-    default:
-        slog.Error("internal/config/config.go", "Message", "env var must be set to either true or false", "var", key)
-        os.Exit(1)
-    }
+	val := strings.TrimSpace(os.Getenv(key))
+	switch strings.ToLower(val) {
+	case "true":
+		*dest = true
+	case "false":
+		*dest = false
+	default:
+		slog.Error("internal/config/config.go", "Message", "env var must be set to either true or false", "var", key)
+		os.Exit(1)
+	}
 }
-
 func (c *Config) SetCookieSameSite() {
-    val := strings.TrimSpace(strings.ToLower(os.Getenv("COOKIE_SAME_SITE")))
-    switch val {
-    case "lax":
-        c.CookieSameSite = http.SameSiteLaxMode
-    case "strict":
-        c.CookieSameSite = http.SameSiteStrictMode
-    case "none":
-        c.CookieSameSite = http.SameSiteNoneMode
-    default:
-        slog.Error("internal/config/config.go", "Message", "COOKIE_SAME_SITE must be lax, strict or none")
-        os.Exit(1)
-    }
+	val := strings.TrimSpace(strings.ToLower(os.Getenv("COOKIE_SAME_SITE")))
+	switch val {
+	case "lax":
+		c.CookieSameSite = http.SameSiteLaxMode
+	case "strict":
+		c.CookieSameSite = http.SameSiteStrictMode
+	case "none":
+		c.CookieSameSite = http.SameSiteNoneMode
+	default:
+		slog.Error("internal/config/config.go", "Message", "COOKIE_SAME_SITE must be lax, strict or none")
+		os.Exit(1)
+	}
 }
 
-func (c *Config) GetPort() string			{ return c.Port }
-func (c *Config) GetInstanceName() string		{ return c.InstanceName }
-func (c *Config) GetLogLevel() slog.Level		{ return c.LogLevel }
-func (c *Config) GetAccessTokenSecret() string		{ return c.AccessTokenSecret }
-func (c *Config) GetRefreshTokenSecret() string		{ return c.RefreshTokenSecret }
-func (c *Config) GetEmailLoginEncryptKey() string	{ return c.EmailLoginEncryptKey }
-func (c *Config) GetAccessTokenTTL() time.Duration	{ return c.AccessTokenTTL }
-func (c *Config) GetRefreshTokenTTL() time.Duration	{ return c.RefreshTokenTTL }
-func (c *Config) GetRedisAddress() string		{ return c.RedisAddress }
-func (c *Config) GetRedisPassword() string		{ return c.RedisPassword }
-func (c *Config) GetPostgresUser() string		{ return c.PostgresUser }
-func (c *Config) GetPostgresPassword() string		{ return c.PostgresPassword }
-func (c *Config) GetPostgresDB() string			{ return c.PostgresDB }
-func (c *Config) GetPostgresHost() string		{ return c.PostgresHost }
+func (c *Config) GetPort()			string		{ return c.Port }
+func (c *Config) GetInstanceName()		string		{ return c.InstanceName }
+func (c *Config) GetLogLevel()			slog.Level	{ return c.LogLevel }
+func (c *Config) GetAccessTokenSecret()		string		{ return c.AccessTokenSecret }
+func (c *Config) GetRefreshTokenSecret()	string		{ return c.RefreshTokenSecret }
+func (c *Config) GetEmailLoginEncryptKey()	string		{ return c.EmailLoginEncryptKey }
+func (c *Config) GetAccessTokenTTL()		time.Duration	{ return c.AccessTokenTTL }
+func (c *Config) GetRefreshTokenTTL()		time.Duration	{ return c.RefreshTokenTTL }
+func (c *Config) GetRedisAddress()		string		{ return c.RedisAddress }
+func (c *Config) GetRedisPassword()		string		{ return c.RedisPassword }
+func (c *Config) GetPostgresUser()		string		{ return c.PostgresUser }
+func (c *Config) GetPostgresPassword()		string		{ return c.PostgresPassword }
+func (c *Config) GetPostgresDB()		string		{ return c.PostgresDB }
+func (c *Config) GetPostgresHost()		string		{ return c.PostgresHost }
+func (c *Config) GetTTSOpenAIURL()		string   	{ return c.TTSOpenAIURL }
+func (c *Config) GetTTSOpenAIModel()		string		{ return c.TTSOpenAIModel }
+func (c *Config) GetTTSOpenAIVoice()		string		{ return c.TTSOpenAIVoice }
+func (c *Config) GetTTSOpenAIKey()		string		{ return c.TTSOpenAIKey }
 
 func New() *Config {
 	c := &Config{}
@@ -140,6 +148,11 @@ func New() *Config {
 
 	c.SetCookieSameSite()
 	c.SetBool("COOKIE_SECURE", &c.CookieSecure)
+
+	c.Set("TTS_OPENAI_URL", &c.TTSOpenAIURL)
+	c.Set("TTS_OPENAI_MODEL_NAME", &c.TTSOpenAIModel)
+	c.Set("TTS_OPENAI_VOICE", &c.TTSOpenAIVoice)
+	c.Set("TTS_OPENAI_API_KEY", &c.TTSOpenAIKey)
 
 	return c
 }
